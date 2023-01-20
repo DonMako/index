0dd40c987f8f7f8d082b9232fd493cd9345049e7
Don Mako <lucas0203@orange.fr>
2023-01-20 16:41:18 +0100

    Début de travail sur les tests

--- tests/index_tests.py
+++ tests/index_tests.py
@@ -4,10 +4,3 @@ def test_open_json():
 
     return ( type(index.open_json("crawled_json")) == list )
 
-
-test_open_json()
-
-
-
-def test_decrement(self):
-    self.assertEqual(inc_dec.decrement(3), 4)
