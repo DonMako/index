bee0fc0ffe1b217be6cb604056e577f981ce74c1
Don Mako <lucas0203@orange.fr>
2023-01-20 15:55:54 +0100

    Mise à jour du programme

--- index/index.py
+++ index/index.py
@@ -1,5 +1,15 @@
 from urllib import request
 from bs4 import BeautifulSoup as BS
+import json
+
+
+
+def open_json(json_file):
+
+    with open(json_file, "r") as f:
+        data = json.load(f)
+    
+    return data
 
 
 
@@ -18,6 +28,22 @@ def get_title(url):
     return title
 
 
+
 def tokenise_simple(title):
 
     return title.split(" ")
+
+
+
+def 
+
+    with open("metadata.json", "w") as f:
+        json.dump(stats, f)
+
+
+
+
+def write_index(index):
+    
+    with open("title.non_pos_index.json", "w") as f:
+        json.dump(index,f)
